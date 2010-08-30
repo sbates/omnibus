2f46ad9cc365eb32452efe9d8750db649c0f952c
Adam Jacob <adam@opscode.com>
2010-08-29 20:34:28 -0700

    Adding in missing source files

--- config/software/gdbm.clj
+++ config/software/gdbm.clj
@@ -1,3 +1,5 @@
+
+
 (software "gdbm" :source "gdbm-1.8.3"
                  :steps [["./configure" "--prefix=/opt/opscode/embedded"]
                          (if (is-os? "darwin") ["perl" "-pi" "-e" "s/BINOWN = bin/BINOWN = root/g" "Makefile"])
