46253a584bf8c9abe24f84116415b6b7004c5699
Adam Jacob <adam@opscode.com>
2011-02-17 11:58:32 -0800

    Removing runit, adding ree RPATH

--- config/software/ree.clj
+++ config/software/ree.clj
@@ -23,6 +23,6 @@
                                                             (is-os? "linux") "./source/distro/google-perftools-1.4/.libs/libtcmalloc_minimal.*") " /opt/opscode/embedded/lib")]
                   ["bash" "-c" "cd ./source && ./configure --prefix=/opt/opscode/embedded --enable-mbari-api CFLAGS='-g -O2' --with-opt-dir=/opt/opscode/embedded"]
                   ["bash" "-c" 
-                   (cond (is-os? "darwin") "cd ./source && make PRELIBS=\"-Wl,-rpath,/opt/opscode/embedded/lib -L/opt/opscode/embedded/lib -lsystem_allocator -ltcmalloc_minimal\""
-                         (is-os? "linux") "cd ./source && make PRELIBS=\"-Wl,-rpath,/opt/opscode/embedded/lib -L/opt/opscode/embedded/lib -ltcmalloc_minimal\"")]
-                  [ "bash" "-c" "cd ./source && make install"]])
+                   (cond (is-os? "darwin") "cd ./source && make RPATH=/opt/opscode/embedded/lib PRELIBS=\"-Wl,-rpath,/opt/opscode/embedded/lib -L/opt/opscode/embedded/lib -lsystem_allocator -ltcmalloc_minimal\""
+                         (is-os? "linux") "cd ./source && make RPATH=/opt/opscode/embedded/lib PRELIBS=\"-Wl,-rpath,/opt/opscode/embedded/lib -L/opt/opscode/embedded/lib -ltcmalloc_minimal\"")]
+                  [ "bash" "-c" "cd ./source && env RPATH=/opt/opscode/embedded/lib make install"]])
