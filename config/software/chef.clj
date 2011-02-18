21d622424b96a63fc3ffeb6ff576ab925df1bb5e
Adam Jacob <adam@opscode.com>
2011-02-17 17:12:32 -0800

    Updating Chef and REE

--- config/software/chef.clj
+++ config/software/chef.clj
@@ -2,6 +2,6 @@
                  :steps [[ "/opt/opscode/embedded/bin/gem" "install" "chef" "fog" "highline" "net-ssh-multi" "-n" "/opt/opscode/bin" "--no-rdoc" "--no-ri"]
                          [ "cp" "setup.rb" "/opt/opscode"]
                          [ "chmod" "755" "/opt/opscode/setup.rb"]
-                         [ "rm" "-rf" "/opt/opscode/embedded/docs"]
-                         [ "chown" "-R" (cond (is-os? "darwin") "root:wheel" true "root:root") "/opt/opscode"]])
+                         [ "rm" "-rf" "/opt/opscode/embedded/docs"]])
+                         ; [ "chown" "-R" (cond (is-os? "darwin") "root:wheel" true "root:root") "/opt/opscode"]])
 
