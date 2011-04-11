434843e0d0cc0156da8290cd360ae50ee8b1f2f5
Christopher Brown <cb@opscode.com>
2011-04-11 11:01:47 -0700

    updating erlang for linux

--- config/software/erlang.clj
+++ config/software/erlang.clj
@@ -17,30 +17,48 @@
 ;; limitations under the License.
 ;;
 
-(software "erlang" :source "otp_src_R14B02"
-          :steps [
-                  {:command "touch" :args ["lib/wx/SKIP"] }                  
-                  {:command "./configure"
-                   :args ["--prefix=/opt/opscode/embedded"
-                          "--enable-threads"
-                          "--enable-smp-support"
-                          "--enable-kernel-poll"
-                          "--enable-darwin-64bit"
-                          "--enable-dynamic-ssl-lib"
-                          "--enable-m64-build"
-                          "--without-javac"
-                          "--with-ssl=/opt/opscode/embedded"
-                          "--build=i686-apple-darwin10"
-                          "--disable-hipe"
-                          "--disable-debug"]
-                   :env { "CFLAGS" "-arch x86_64 -m64 -L/opt/opscode/embedded/lib -I/opt/opscode/embedded/include"
-                          "LDFLAGS" "-arch x86_64 -R/opt/opscode/embedded/lib -L/opt/opscode/embedded/lib -I/opt/opscode/embedded/include"
-                          }}
-                  {
-                   :env { "CFLAGS" "-arch x86_64 -m64 -L/opt/opscode/embedded/lib -I/opt/opscode/embedded/include"
-                          "LDFLAGS" "-arch x86_64 -R/opt/opscode/embedded/lib -L/opt/opscode/embedded/lib -I/opt/opscode/embedded/include" }
-                   :command "make"
-                   }
-                  {:command "make" :args ["install"]}])
+(let [args (cond
+            (and (is-os? "darwin") (is-machine? "x86_64"))
+            ["--prefix=/opt/opscode/embedded"
+             "--enable-threads"
+             "--enable-smp-support"
+             "--enable-kernel-poll"
+             "--enable-darwin-64bit"
+             "--enable-dynamic-ssl-lib"
+             "--enable-m64-build"
+             "--without-javac"
+             "--with-ssl=/opt/opscode/embedded"
+             "--build=i686-apple-darwin10"
+             "--disable-hipe"
+             "--disable-debug"]
+            (is-os? "linux")
+            ["--prefix=/opt/opscode/embedded"
+             "--enable-threads"
+             "--enable-smp-support"
+             "--enable-kernel-poll"
+             "--enable-dynamic-ssl-lib"
+             "--enable-hipe"
+             "--without-javac"
+             "--with-ssl=/opt/opscode/embedded"
+             "--disable-debug"])
+      env (cond
+           (and (is-os? "darwin" (is-machine? "x86_64")))
+           { "CFLAGS" "-arch x86_64 -m64 -L/opt/opscode/embedded/lib -I/opt/opscode/embedded/include"
+             "LDFLAGS" "-arch x86_64 -R/opt/opscode/embedded/lib -L/opt/opscode/embedded/lib -I/opt/opscode/embedded/include"}
+           (is-os? "linux")
+           { "CFLAGS" "-L/opt/opscode/embedded/lib -I/opt/opscode/embedded/include"
+             "LDFLAGS" "-R/opt/opscode/embedded/lib -L/opt/opscode/embedded/lib -I/opt/opscode/embedded/include"}
+           )
+      ]
+  (software "erlang" :source "otp_src_R14B02"
+            :steps [
+                    {:command "touch" :args ["lib/wx/SKIP"] }                  
+                    {:command "./configure"
+                     :args args
+                     :env env}
+                    {
+                     :env env
+                     :command "make"
+                     }
+                    {:command "make" :args ["install"]}]))
 
-;;                          "--enable-hipe"
