b1dc0685f42f2b2ad57d6e1322d1533e91757db0
Adam Jacob <adam@opscode.com>
2011-05-27 23:39:58 -0700

    Postinst, not postinstall

--- src/fatty/core.clj
+++ src/fatty/core.clj
@@ -98,7 +98,7 @@
 (defn build-rpm
   "Builds a rpm"
   [project-name version os-data]
-  (let [status (sh "fpm" "-s" "dir" "-t" "rpm" "-v" version "-n" project-name "/opt/opscode" "-m" "Opscode, Inc." "--post-install" "../source/postinstall" "--post-uninstall" "../source/postrm" "--description" "The full stack install of Opscode Chef" "--url" "http://www.opscode.com" :dir "./pkg")]
+  (let [status (sh "fpm" "-s" "dir" "-t" "rpm" "-v" version "-n" project-name "/opt/opscode" "-m" "Opscode, Inc." "--post-install" "../source/postinst" "--post-uninstall" "../source/postrm" "--description" "The full stack install of Opscode Chef" "--url" "http://www.opscode.com" :dir "./pkg")]
     (log-sh-result status
                    (str "Created rpm package")
                    (str "Failed to create rpm package"))))
