bd7db18c40c693fef4d193ab1e69e9a12c1692ba
Adam Jacob <adam@opscode.com>
2011-06-24 13:56:06 -0700

    Adding special rpath sauce

--- config/software/db.clj
+++ config/software/db.clj
@@ -17,11 +17,23 @@
 ;; limitations under the License.
 ;;
 
-(software "db" :source "db-5.0.26.NC"
-               :build-subdir "build_unix"
-               :steps [{:env {
-                              "LDFLAGS" "-R/opt/opscode/embedded/lib -L/opt/opscode/embedded/lib -I/opt/opscode/embedded/include"
-                              "CFLAGS" "-L/opt/opscode/embedded/lib -I/opt/opscode/embedded/include"}
-                        :command "../dist/configure" :args ["--prefix=/opt/opscode/embedded"]}
-                       {:command "make"}
-                       {:command "make" :args ["install"]}])
+(let [env
+      (cond
+       (and (is-os? "darwin") (is-machine? "x86_64"))
+       { "CFLAGS" "-L/opt/opscode/embedded/lib -I/opt/opscode/embedded/include"
+         "LDFLAGS" "-R/opt/opscode/embedded/lib -L/opt/opscode/embedded/lib -I/opt/opscode/embedded/include"}
+       (is-os? "linux")
+       { "CFLAGS" "-L/opt/opscode/embedded/lib -I/opt/opscode/embedded/include"
+         "LDFLAGS" "-Wl,-rpath /opt/opscode/embedded/lib -L/opt/opscode/embedded/lib -I/opt/opscode/embedded/include"}
+       (is-os? "solaris2")
+       { "CFLAGS" "-L/opt/opscode/embedded/lib -I/opt/opscode/embedded/include"
+         "LDFLAGS" "-Wl,-rpath /opt/opscode/embedded/lib -L/opt/opscode/embedded/lib -I/opt/opscode/embedded/include"}
+       )
+      ]
+
+  (software "db" :source "db-5.0.26.NC"
+                 :build-subdir "build_unix"
+                 :steps [{:env env
+                          :command "../dist/configure" :args ["--prefix=/opt/opscode/embedded"]}
+                         {:command "make"}
+                         {:command "make" :args ["install"]}]))
