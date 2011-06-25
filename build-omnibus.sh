ec5cfabbc49d770d3cd5b773c188a2c8431bedcc
Adam Jacob <adam@opscode.com>
2011-06-25 14:37:11 -0700

    Working the crazy path

--- build-omnibus.sh
+++ build-omnibus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export LEIN_ROOT=true
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export PATH=/opt/opscode/embedded/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/gnu/bin:/usr/sfw/bin:/usr/gcc/4.3/bin:/var/ruby/1.8/gem_home/bin
 cd /root/omnibus
 git pull >/tmp/omnibus.out 2>&1 
 lein run --project-name "$1" --bucket-name "$2" --s3-access-key "$3" --s3-secret-key "$4" >>/tmp/omnibus.out 2>&1 
